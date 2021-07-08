b4b8aa6a6836f6b9a8f0022130d0b1f6d49174cd
Cesar Rodriguez <streamlink777@live.com>
2021-07-08 15:53:22 -0400

    barcode scanner

--- lib/Screens/Dashboard.dart
+++ lib/Screens/Dashboard.dart
@@ -1,8 +1,8 @@
 import 'dart:developer';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:google_ml_vision/google_ml_vision.dart';
 import 'package:image_picker/image_picker.dart';
+import 'package:google_ml_vision/google_ml_vision.dart';
 
 
 class DashBoard extends StatefulWidget {
@@ -15,47 +15,64 @@ class DashBoard extends StatefulWidget {
 
 class _DashBoardState extends State<DashBoard> {
 
-  void takePictures() async{
-    final _picker = ImagePicker();
-    final pickedFile = await _picker.getImage(source: ImageSource.camera);
-    final bytes = await pickedFile!.readAsBytes();
-    log(bytes.toString());
+  late final _image;
+
+
+  _getBarcodeCode()async{
+    final GoogleVisionImage visionImage = GoogleVisionImage.fromFile(_image);
+    final BarcodeDetector barcodeDetector = GoogleVision.instance.barcodeDetector();
+    final List<Barcode> barcodes = await barcodeDetector.detectInImage(visionImage);
+    for (Barcode barcode in barcodes) {
+      final rawValue = barcode.rawValue;
+      log(rawValue.toString());
+    }
   }
 
-  Future<String> barcodeDetector()async{
-    takePictures();
-    // final GoogleVisionImage visionImage = GoogleVisionImage.fromFile(imageFile);
-    // final BarcodeDetector barcodeDetector = GoogleVision.instance.barcodeDetector();
-    // final List<Barcode> barcodes = await barcodeDetector.detectInImage(visionImage);
-    // var rawValue;
-    // for (Barcode barcode in barcodes) {
-    //   rawValue = barcode.rawValue;
-    // }
-    // return rawValue;
-    return 'hello';
+  final ImagePicker _picker = ImagePicker();
+  _imgFromCamera() async {
+    final image = await _picker.getImage(source: ImageSource.camera, imageQuality: 50);
+
+    setState(() {
+      _image = image;
+    });
+    _getBarcodeCode();
   }
 
-  String barcode = '';
+  _imgFromGallery() async {
+    final image = await _picker.getImage(
+        source: ImageSource.gallery, imageQuality: 50
+    );
+
+    setState(() {
+      _image = image;
+    });
+
+    _getBarcodeCode();
+  }
+
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         body: SafeArea(
-          child: Center(
-            child: ElevatedButton(
-              style: ElevatedButton.styleFrom(
-                  primary: Colors.blue,
-                  padding: EdgeInsets.symmetric(horizontal: 50, vertical: 20),
-                  textStyle: TextStyle(
-                      fontSize: 30,
-                      fontWeight: FontWeight.bold),
-              ),
-              onPressed: (){
-                log('hello world');
-                barcodeDetector();
-              },
-              child: Text('Receive Item'),
+          child: Container(
+            child: Wrap(
+              children: <Widget>[
+                ListTile(
+                    leading: new Icon(Icons.photo_library),
+                    title: new Text('Photo Library'),
+                    onTap: () {
+                      _imgFromGallery();
+                    }),
+                ListTile(
+                  leading: new Icon(Icons.photo_camera),
+                  title: new Text('Camera'),
+                  onTap: () {
+                    _imgFromCamera();
+                  },
+                ),
+              ],
             ),
           ),
         ),
